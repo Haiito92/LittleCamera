4c789f9a602654211d95fe52d64159f877f7cfa1
Haiito92 <hannaantoinebusiness@gmail.com>
2025-09-08 16:02:11 +0200

    [GP] Pitch max offset, Yaw max offset (bug at 180 limit)

--- Assets/LittleCamera/Scripts/Runtime/Views/FixedFollowView.cs
+++ Assets/LittleCamera/Scripts/Runtime/Views/FixedFollowView.cs
@@ -10,16 +10,22 @@ namespace LittleCamera.Views
         [SerializeField] private float _fieldOfView;
         [SerializeField] private Transform _target;
         
+        // Constrain Zone
+        [SerializeField] private Transform _centralPoint;
+        [SerializeField] private float _yawOffsetMax;
+        [SerializeField] private float _pitchOffsetMax;
+        
         public override CameraConfiguration GetConfiguration()
         {
             CameraConfiguration configuration = new CameraConfiguration();
 
-            if (_target == null) return configuration;
+            if (_target == null || _centralPoint == null) return configuration;
             
-            Vector3 targetDirection = (_target.transform.position - transform.position).normalized;
+            Vector3 centralPointDirection = (_centralPoint.position - transform.position).normalized;
+            Vector3 targetDirection = (_target.position - transform.position).normalized;
             
-            configuration.Yaw = Mathf.Atan2(targetDirection.x, targetDirection.z) * Mathf.Rad2Deg;
-            configuration.Pitch = -Mathf.Asin(targetDirection.y) * Mathf.Rad2Deg;
+            configuration.Yaw = ComputeYaw(centralPointDirection, targetDirection);
+            configuration.Pitch = ComputePitch(centralPointDirection, targetDirection);
             configuration.Roll = _roll;
             
             configuration.Distance = 0;
@@ -28,6 +34,38 @@ namespace LittleCamera.Views
             configuration.FieldOfView = _fieldOfView;
 
             return configuration;
+        }
+
+        private float ComputeYaw(Vector3 centralPointDirection, Vector3 targetDirection)
+        {
+            float centralYaw = Mathf.Atan2(centralPointDirection.x, centralPointDirection.z) * Mathf.Rad2Deg; 
+            float targetYaw = Mathf.Atan2(targetDirection.x, targetDirection.z) * Mathf.Rad2Deg;
+
+            float diff = targetYaw - centralYaw;
+            float absDiff = Mathf.Abs(diff);
+
+            if (absDiff < _yawOffsetMax)
+            {
+                return targetYaw;
+            }
+
+            return diff > 0 ? centralYaw + _yawOffsetMax : centralYaw - _yawOffsetMax;
+        }
+        
+        private float ComputePitch(Vector3 centralPointDirection, Vector3 targetDirection)
+        {
+            float centralPitch = -Mathf.Asin(centralPointDirection.y) * Mathf.Rad2Deg;
+            float targetPitch = -Mathf.Asin(targetDirection.y) * Mathf.Rad2Deg;
+
+            float diff = targetPitch - centralPitch;
+            float absDiff = Mathf.Abs(diff);
+
+            if (absDiff < _pitchOffsetMax)
+            {
+                return targetPitch;
+            }
+
+            return diff > 0 ? centralPitch + _pitchOffsetMax : centralPitch - _pitchOffsetMax;
         }
     }
 }
