8d9de6a0a3e4641213ad701b5330a1d571a3618a
Haiito92 <hannaantoinebusiness@gmail.com>
2025-09-09 11:12:31 +0200

    [GP] MathUtils Implementation

--- Assets/LittleCamera/Scripts/Runtime/Utils/MathsUtils.cs
+++ Assets/LittleCamera/Scripts/Runtime/Utils/MathsUtils.cs
@@ -6,17 +6,17 @@ namespace LittleCamera.Utils
     {
         public static Vector3 LinearBezier(Vector3 a, Vector3 b, float t)
         {
-            throw new System.NotImplementedException();
+            return (1 - t) * a + t * b;
         }
         
         public static Vector3 QuadraticBezier(Vector3 a, Vector3 b, Vector3 c, float t)
         {
-            throw new System.NotImplementedException();
+            return (1 - t) * LinearBezier(a, b, t) + t * LinearBezier(b, c, t);
         }
         
         public static Vector3 CubicBezier(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t)
         {
-            throw new System.NotImplementedException();
+            return (1 - t) * QuadraticBezier(a, b, c, t) + t * QuadraticBezier(b, c, d, t);
         }
     }
 }
